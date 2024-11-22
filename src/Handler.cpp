8bdf2900f28307770607ae720a7c5f722821961c
YuChen Ding <yuchen.ding@bytedance.com>
2024-11-22 15:57:58 +0800

    implement echo

--- src/Handler.cpp
+++ src/Handler.cpp
@@ -2,23 +2,86 @@
 #include <cstddef>
 #include <iostream>
 #include <sstream>
+#include <string>
+#include <sys/_types/_ssize_t.h>
 #include <sys/socket.h>
 #include <vector>
 
 class Handler {
 private:
   int _client_socket;
-  std::array<std::byte, 1024> buffer{};
   std::vector<char> msg_buffer{};
   size_t idx;
 
-  void read_until(size_t size) {}
+  ssize_t read_until(size_t size, std::vector<char> &buf) {
+    return recv(size, buf.data(), buf.size(), 0);
+  }
 
-  void read_until(std::string delimiter) {}
+  std::vector<std::string> handle(std::stringstream &ss) {
+    char b;
+    ss.read(&b, 1);
+    if ('*' == b) {
+      return handle_aggregate(ss);
+    } else if ('$' == b) {
+      return handle_bulk_string(ss);
+    }
+    return {};
+  }
+  
+  void read_delim(std::stringstream& ss) {
+    char c[2];
+    ss.read(c, 2);
+  }
 
-  void handle_aggregate() {}
+  std::vector<std::string> handle_aggregate(std::stringstream &ss) {
+    int size;
+    ss >> size;
+    read_delim(ss);
+    std::vector<std::string> strings;
+    for (size_t i = 0; i < size; i++) {
+      auto res = handle(ss);
+      strings.insert(strings.end(), res.begin(), res.end());
+    }
+    if (strings.empty()) {
+      return strings;
+    }
 
-  void handle_bulk_string() {}
+    std::stringstream out;
+    if (strings.front() == "ECHO") {
+      resp(out, strings.back());
+    } else {
+      resp(out, "PONG");
+    }
+
+    std::string res = out.str();
+    send(_client_socket, res.data(), res.size(), 0);
+    return strings;
+  }
+  
+  // to overload between bulkstring and simple strings
+  void resp(std::stringstream& out, const std::string& bulk_string, size_t size) {
+    out << '$';
+    out << size << "\r\n";
+    out << bulk_string;
+    out << "\r\n";
+  }
+  
+  void resp(std::stringstream& out, const std::string& simple_string) {
+    out << '+';
+    out << simple_string << "\r\n";
+  }
+
+  std::vector<std::string> handle_bulk_string(std::stringstream &ss) {
+    int size;
+    ss >> size;
+    std::string word;
+    word.resize(size);
+    read_delim(ss);
+    ss.read(word.data(), size);
+    read_delim(ss);
+    // std::cout << "received_string: " << word << std::endl;
+    return {word};
+  }
 
 public:
   Handler(int client_socket) {
@@ -27,23 +90,14 @@ public:
   }
 
   void handle() {
+    std::array<std::byte, 1024> buffer{};
     while (ssize_t size =
                recv(_client_socket, buffer.data(), buffer.size(), 0)) {
       std::stringstream ss{};
       for (size_t i = 0; i < size; i++) {
         ss << static_cast<char>(buffer[i]);
       }
-      char b;
-      ss >> b;
-      if ('*' == b) {
-        handle_aggregate();
-      } else if ('$' == b) {
-        handle_bulk_string();
-      }
-      std::cout << b << std::endl;
-
-      std::string pong = "+PONG\r\n";
-      send(_client_socket, pong.data(), pong.size(), 0);
+      handle(ss);
     }
   }
 };
